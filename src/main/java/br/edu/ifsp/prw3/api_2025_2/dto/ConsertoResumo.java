e9ef9f35c3b51b71d55ba1853576bfd50bfc6eb0
LVSigoli <lucasviniciussigoli@gmail.com>
2025-04-18 18:11:46 -0300

    refactor: Add id to consertoResumo repository

--- src/main/java/br/edu/ifsp/prw3/api_2025_2/dto/ConsertoResumo.java
+++ src/main/java/br/edu/ifsp/prw3/api_2025_2/dto/ConsertoResumo.java
@@ -2,8 +2,8 @@ package br.edu.ifsp.prw3.api_2025_2.dto;
 
 import br.edu.ifsp.prw3.api_2025_2.models.Conserto;
 
-public record ConsertoResumo(String dataEntrada, String dataSaida, String nomeMecanico, String marca, String modelo) {
+public record ConsertoResumo(Long id, String dataEntrada, String dataSaida, String nomeMecanico, String marca, String modelo) {
     public ConsertoResumo(Conserto c) {
-        this(c.getDataEntrada(), c.getDataSaida(), c.getMecanico().getNome(), c.getVeiculo().getMarca(), c.getVeiculo().getModelo());
+        this(c.getId(), c.getDataEntrada(), c.getDataSaida(), c.getMecanico().getNome(), c.getVeiculo().getMarca(), c.getVeiculo().getModelo());
     }
 }
