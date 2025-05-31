f7e21c8cf2f16bc67971cea3944b3f5c57561afd
LVSigoli <lucasviniciussigoli@gmail.com>
2025-05-31 20:54:02 -0300

    feat: Implement final user class

--- src/main/java/br/edu/ifsp/prw3/api_2025_2/models/Usuario.java
+++ src/main/java/br/edu/ifsp/prw3/api_2025_2/models/Usuario.java
@@ -1,13 +1,56 @@
 package br.edu.ifsp.prw3.api_2025_2.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class Usuario {
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String login;
     private  String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
